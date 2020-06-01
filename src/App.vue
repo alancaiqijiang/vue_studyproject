7e8af89d6690bbb36558906fa2b4d631775fdc87
alancaiqijiang <alancaiqijiang@gmail.com>
2020-06-01 22:50:34 +0800

    看完视频尝试自己从手册上搞过来

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <div>
-    <h1>哈哈哈哈哈</h1>
-    <DatePicker />
+  <div class="wrap">
+    <h1>二手车市场</h1>
+    <Table :columns="className" :data="sCarData"></Table>
   </div>
 </template>
 
 <script>
-
-
+export default {
+  data() {
+    return {
+      className: [
+        {
+          title: "Name",
+          key: "name"
+        },
+        {
+          title: "Age",
+          key: "age"
+        },
+        {
+          title: "Address",
+          key: "address"
+        }
+      ],
+      sCarData: [
+        {
+          name: "John Brown",
+          age: 18,
+          address: "New York No. 1 Lake Park",
+          date: "2016-10-03"
+        },
+        {
+          name: "Jim Green",
+          age: 24,
+          address: "London No. 1 Lake Park",
+          date: "2016-10-01"
+        },
+        {
+          name: "Joe Black",
+          age: 30,
+          address: "Sydney No. 1 Lake Park",
+          date: "2016-10-02"
+        },
+        {
+          name: "Jon Snow",
+          age: 26,
+          address: "Ottawa No. 2 Lake Park",
+          date: "2016-10-04"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
+.wrap {
+  width: 1000px;
+  margin: 50px auto;
+}
+h1 {
+  font-size: 30px;
+  text-align: center;
+}
 </style>
