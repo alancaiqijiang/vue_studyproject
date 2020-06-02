32e19a5c55b8ac12eca373b438e56c6990041739
alancaiqijiang <alancaiqijiang@gmail.com>
2020-06-02 16:54:49 +0800

    homework finished

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="wrap">
+    <!-- 左边得界面 -->
+    <div class="nav">
+      <ul>
+        <li>
+          <a href>
+            <img src="/images/icon/icon1.svg" alt />
+          </a>
+        </li>
+        <li>
+          <a href>
+            <img src="/images/icon/icon2.svg" alt />
+          </a>
+        </li>
+        <li>
+          <a href>
+            <img src="/images/icon/icon3.svg" alt />
+          </a>
+        </li>
+        <li>
+          <a href>
+            <img src="/images/icon/icon4.svg" alt />
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- 右边得主界面 -->
     <div class="dashboard">
       <h1>Billing Address</h1>
       <div class="sider-bar">
@@ -18,45 +44,94 @@
           </li>
         </ul>
       </div>
+      <!-- 选项卡 -->
       <div class="main">
         <Row :gutter="16">
-          <i-col span="8">
-            <Card :bordered="true">
-                <p slot="title">card</p>
-                <p>Content of card</p>
-                <p>Content of card</p>
-                <p>Content of card</p>
+          <i-col span="8" v-for="(item, index) in addressArr" :key="index">
+            <Card :bordered="true" style="height:180px;">
+              <div slot="title">
+                <b>{{item.alias}}</b>
+              </div>
+              <div slot="extra">
+                <a href>Delete</a>
+                <a href>Modify</a>
+              </div>
+              <p>
+                <b>Name:</b>
+                {{item.n}}
+              </p>
+              <p>
+                <b>Tel:</b>
+                {{item.tel}}
+              </p>
+              <p>
+                <b>Address:</b>
+                {{item.p}} {{item.c}} {{item.a}} {{item.s}} {{item.d}}
+              </p>
             </Card>
           </i-col>
-          <i-col span="8">
-            <Card :bordered="true">
-                <p slot="title"> card</p>
-                <p>Content of card</p>
-                <p>Content of card</p>
-                <p>Content of card</p>
-            </Card>
-          </i-col>
-          <i-col span="8">
-            <Card :bordered="true">
-                <p slot="title">card</p>
-                <p>Content of card</p>
-                <p>Content of card</p>
-                <p>Content of card</p>
+          <i-col span="8" v-if="addressArr.length != 3">
+            <Card :bordered="true" style="height:180px;text-align:center;font-size:100px;">
+              <span class="plus" @click="isChoosePCAS = true">+</span>
             </Card>
           </i-col>
         </Row>
       </div>
     </div>
+    <!--主界面结束  -->
+    <!--弹出层  -->
+    <Modal
+      v-model="isChoosePCAS"
+      width="500"
+      title="Add a new shipping address"
+      @on-ok="ok"
+      @on-cancel="cancel"
+    >
+      <p>
+        <b>
+          <span v-if="s ==''">Please Choose your location </span>
+          <span v-else>{{p}}{{c}}{{a}}{{s}}</span>
+        </b>
+         
+        
+      </p>
+      <Dropdown
+        trigger="custom"
+        :visible="isDropDownvisible"
+        style="margin-left: 20px"
+        placement="bottom-start"
+      >
+        <a href="javascript:void(0)" @click="handleDropOpen">
+          Province, City, Area, Street
+          <Icon type="ios-arrow-down"></Icon>
+        </a>
+        <DropdownMenu slot="list" style="height:200px; width:600px">
+          <div class="dropDownInnerBox">
+            <DropdownInner @fulladress="fulladress" />
+          </div>
+          <div style="text-align: right;margin:10px;">
+            <Button type="primary" @click="handleDropClose">关闭</Button>
+          </div>
+        </DropdownMenu>
+      </Dropdown>
+    </Modal>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import DropdownInner from "./components/dropDownInner.vue";
 export default {
   data() {
     return {
       // 准备一个空数组
-      addressArr: []
+      addressArr: [],
+      isChoosePCAS: true,
+      isDropDownvisible: true,
+      p: "",
+      c: "",
+      a: "",
+      s: ""
     };
   },
   created() {
@@ -66,21 +141,88 @@ export default {
       // console.log(data.data)
       this.addressArr = data.data;
     });
+  },
+  methods: {
+    ok() {
+      this.$Message.info("Clicked ok");
+    },
+    cancel() {
+      this.$Message.info("Clicked cancel");
+    },
+    handleDropOpen() {
+      this.isDropDownvisible = true;
+    },
+    handleDropClose() {
+      this.isDropDownvisible = false;
+    },
+    fulladress(obj) {
+      this.p = obj.p;
+      this.c = obj.c;
+      this.a = obj.a;
+      this.s = obj.s;
+      this.isDropDownvisible= false;
+    }
+  },
+  components: {
+    DropdownInner: DropdownInner
   }
 };
 </script>
 
 <style lang="less" scoped>
 .wrap {
-  width: 1020px;
+  width: 1220px;
   height: 960px;
   margin: 50px auto;
-  background-color: #1d212a;
+  background-color: #ffffff;
   border-radius: 50px;
+  overflow: hidden;
+  border: 3px solid #30313c;
+}
+.nav {
+  width: 10%;
+  height: 960px;
+  float: left;
+  background-color: #0068ff;
+  border-bottom-left-radius: 50px;
+  position: relative;
+  ul {
+    width: 100%;
+    height: 100%;
+    list-style: none;
+    position: absolute;
+    left: 40px;
+    top: 190px;
+  }
+  ul li {
+    height: 60px;
+    width: 60px;
+    margin-bottom: 90px;
+  }
+  ul li a {
+    display: block;
+    height: 60px;
+    width: 60px;
+  }
+  ul li img {
+    height: 35px;
+    width: 35px;
+  }
 }
 .dashboard {
-  width: 1020px;
-  height: 180px;
+  width: 90%;
+  height: 960px;
+  float: left;
+  h1 {
+    height: 60px;
+    padding-top: 20px;
+    padding-bottom: 60px;
+    font-size: 30px;
+    line-height: 30px;
+    text-align: center;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+      sans-serif;
+  }
 }
 .sider-bar {
   width: 20%;
@@ -110,16 +252,18 @@ export default {
   }
 }
 .main {
-  width: 80%;
+  width: 70%;
   height: 760px;
   float: left;
+  margin-left: 50px;
+  .plus {
+    cursor: pointer;
+    &:hover {
+      color: #0068ff;
+    }
+  }
 }
-h1 {
-  padding-top: 20px;
-  font-size: 30px;
-  line-height: 30px;
-  text-align: center;
-  color: white;
-  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+.dropDownInnerBox {
+  height: 150px;
 }
 </style>
---
+++ src/components/dropDownInner.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="inner">
+    <p v-if="currentPage == 'prov'">
+      <a
+        v-for="(v,prov) in pcasobj"
+        :key="prov"
+        href="javascript:;"
+        @click="ProvClick(prov)"
+      >{{prov}}</a>
+    </p>
+    <p v-if="currentPage == 'city'">
+      <a
+        v-for="(v,city) in pcasobj[prov]"
+        :key="city"
+        href="javascript:;"
+        @click="CityClick(city)"
+      >{{city}}</a>
+    </p>
+    <p v-if="currentPage == 'area'">
+      <a
+        v-for="(v,area) in pcasobj[prov][city]"
+        :key="area"
+        href="javascript:;"
+        @click="AreaClick(area)"
+      >{{area}}</a>
+    </p>
+    <p v-if="currentPage == 'street'">
+      {{street}}
+      <a
+        v-for="street in pcasobj[prov][city][area]"
+        :key="street"
+        href="javascript:;"
+        @click="StreetClick(street)"
+      >{{street}}</a>
+    </p>
+    <div class="hd">
+      <p>The choosing address:</p>
+      <span :class="{'cur': currentPage == 'prov'}" @click="hd_han('prov')">
+        <em v-if="prov!=''">{{prov}}</em>
+        <em v-else>Please choose a Province</em>
+      </span>
+      <span v-if="prov!=''" :class="{'cur': currentPage == 'city'}" @click="hd_han('city')">
+        <em v-if="city!=''">{{city}}</em>
+        <em v-else>Please choose a city</em>
+      </span>
+      <span v-if="city!=''" :class="{'cur': currentPage == 'area'}" @click="hd_han('area')">
+        <em v-if="area!=''">{{area}}</em>
+        <em v-else>Please choose a area</em>
+      </span>
+      <span v-if="area!=''" :class="{'cur': currentPage == 'street'}" @click="hd_han('street')">
+        <em v-if="street!=''">{{street}}</em>
+        <em v-else>Please choose a street</em>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      pcasobj: {},
+      //当前显示得界面，值为p,c,a,s
+      currentPage: "prov",
+      prov: "",
+      city: "",
+      area: "",
+      street: ""
+    };
+  },
+  created() {
+    // 拉取接口
+    axios.get("http://www.aiqianduan.com:56506/pcas").then(data => {
+      this.pcasobj = data.data;
+    });
+  },
+  methods: {
+    ProvClick(prov) {
+      this.prov = prov;
+      // console.log(this.prov)
+      this.city = "";
+      this.area = "";
+      this.street = "";
+      this.currentPage = "city";
+    },
+    CityClick(city) {
+      this.city = city;
+      this.area = "";
+      this.street = "";
+      this.currentPage = "area";
+    },
+    AreaClick(area) {
+      this.area = area;
+      this.street = "";
+      this.currentPage = "street";
+    },
+    StreetClick(street) {
+      this.street = street;
+      this.$emit("fulladress", {
+        p: this.prov,
+        c: this.city,
+        a: this.area,
+        s: this.street
+        
+      });
+    },
+    hd_han(m) {
+      this.currentPage = m;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.inner {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.hd {
+  border-top: 1px solid #cccccc;
+  width: 600px;
+  height: 40px;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  p {
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+      "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+    font-size: 18px;
+    line-height: 26px;
+    height: 26px;
+    width: 100%;
+    font-weight: 600;
+  }
+  span {
+    float: left;
+    padding: 0 10px;
+    border-bottom: 2px solid #0068ff;
+    margin-top: 5px;
+    margin-left: 10px;
+    font-size: 16px;
+    font-weight: 500;
+    cursor: pointer;
+    font-style: none;
+
+    &.cur {
+      background-color: gold;
+    }
+  }
+}
+a {
+  margin-left: 10px;
+  float: left;
+}
+</style>
